ab525662cf06d4a663ef2e7059f33c7e9fa9107d
owein <owein.dourneaupro@gmail.com>
2020-02-07 15:38:11 +0100

    Creation du fichier quizz.js / list_Theme et listeQuizz

---
+++ data/quizz.js
@@ -0,0 +1,51 @@
+const list_theme = [
+    {
+        id : 1,
+        titre :"Les profs",
+        listeQuiz : [
+            {
+                id : 1,
+                titre : "Quiz 1 ",
+            },
+            {
+                id : 2,
+                titre : "Quiz 2 ",
+            }
+            
+
+        ]
+    },
+    {
+        id : 2,
+        titre : "Les cours",
+        listeQuiz  :  [
+            {
+                id : 1,
+                titre : "Quiz 1 ",
+            },
+            {
+                id : 2,
+                titre : "Quiz 2 ",
+            }
+            
+
+        ]
+    },
+    {
+        id : 3,
+        titre : "L'asso",
+        listeQuiz : [
+            {
+                id : 1,
+                titre : "Quiz 1 ",
+            },
+            {
+                id : 2,
+                titre : "Quiz 2 ",
+            }
+            
+
+        ]
+    }
+   
+];

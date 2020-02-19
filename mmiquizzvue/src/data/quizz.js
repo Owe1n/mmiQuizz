dd616b708d709581cb1fc3686a1818086d009299
Elise Gondange <elisegdg@MacBook-Pro-de-Elise.local>
2020-02-19 10:48:55 +0100

    Récup fontion getTheme et getQuiz de gach

--- mmiquizzvue/src/data/quizz.js
+++ mmiquizzvue/src/data/quizz.js
@@ -1,51 +1,60 @@
 const list_theme = [
     {
-        id : 1,
-        titre :"Les profs",
-        listeQuiz : [
+        id: 1,
+        titre: "Les profs",
+        listeQuiz: [
             {
-                id : 1,
-                titre : "Quiz 1 ",
+                id: 1,
+                titre: "Quiz 1 ",
             },
             {
-                id : 2,
-                titre : "Quiz 2 ",
+                id: 2,
+                titre: "Quiz 2 ",
             }
-            
+
 
         ]
     },
     {
-        id : 2,
-        titre : "Les cours",
-        listeQuiz  :  [
+        id: 2,
+        titre: "Les cours",
+        listeQuiz: [
             {
-                id : 1,
-                titre : "Quiz 1 ",
+                id: 1,
+                titre: "Quiz 1 ",
             },
             {
-                id : 2,
-                titre : "Quiz 2 ",
+                id: 2,
+                titre: "Quiz 2 ",
             }
-            
+
 
         ]
     },
     {
-        id : 3,
-        titre : "L'asso",
-        listeQuiz : [
+        id: 3,
+        titre: "L'asso",
+        listeQuiz: [
             {
-                id : 1,
-                titre : "Quiz 1 ",
+                id: 1,
+                titre: "Quiz 1 ",
             },
             {
-                id : 2,
-                titre : "Quiz 2 ",
+                id: 2,
+                titre: "Quiz 2 ",
             }
-            
+
 
         ]
     }
-   
+
 ];
+
+export function getTheme(id) {
+    return list_theme.find(item => item.id === id);
+}
+
+
+export function getQuiz(id) {
+    return list_theme.listeQuiz.find(item => item.id === id);
+}

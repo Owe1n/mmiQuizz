1b87a2b07dec60a8c26f50757f3d51f0a13ba50a
owein <owein.dourneaupro@gmail.com>
2020-02-20 10:37:36 +0100

    MAJ main.js

--- mmiquizzvue/src/main.js
+++ mmiquizzvue/src/main.js
@@ -9,14 +9,16 @@ new Vue({
 
 
 //
+/*
 async function recupereQuiz(id){
-  const res = await fetch(`../data/quizz${id}/quizz.json`); // On va chercher le quizz.json
+  const res = await fetch(`../src/data/quizz${id}/quizz.json`); // On va chercher le quizz.json
+  console.log(res)
   if(res.ok){
       const json = await res.json(); // Si ça s'est bien passé on va le transformaer en obj JS
       return json
   }
 }
-
+*/
 /* Recupere et affiche dans l'html
 recupereQuiz(1).then( (quizz) => {
   console.log(quizz);

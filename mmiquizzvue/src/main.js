dedf9f04474fc9916ab068dd6127fc0ce5b7e2eb
owein <owein.dourneaupro@gmail.com>
2020-03-22 21:31:16 +0100

    Suppression de l'ancien ajax

--- mmiquizzvue/src/main.js
+++ mmiquizzvue/src/main.js
@@ -5,29 +5,6 @@ Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
 
 
-//
-/*
-async function recupereQuiz(id){
-  const res = await fetch(`../src/data/quizz${id}/quizz.json`); // On va chercher le quizz.json
-  console.log(res)
-  if(res.ok){
-      const json = await res.json(); // Si ça s'est bien passé on va le transformaer en obj JS
-      return json
-  }
-}
-*/
-/* Recupere et affiche dans l'html
-recupereQuiz(1).then( (quizz) => {
-  console.log(quizz);
-  console.log (quizz.questions);
-  let questionsVu = document.querySelector('#questions');
-  quizz.questions.forEach(question => {
-      
-      questionsVu.innerHTML = questionsVu.innerHTML + "<br>" + question.titre
-
-  });
-})
-*/

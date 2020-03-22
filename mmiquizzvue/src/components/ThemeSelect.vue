6c523d2d680f14dc12490756ab6ad62b9e3ac4ce
Elise Gondange <elisegdg@MBP-de-Elise.gondix.fr>
2020-03-22 19:01:30 +0100

    Ajout style interface gauche

--- mmiquizzvue/src/components/ThemeSelect.vue
+++ mmiquizzvue/src/components/ThemeSelect.vue
@@ -1,18 +1,27 @@
 <template>
-  <div>
-    <h2 class="theme-title">Thème</h2>
-    <button
-      class="btn btn-primary d-block"
-      @click="montrerListe = !montrerListe"
-    >{{ theme ? theme.titre : "Choisir un thème..." }}</button>
-    <div class="btn-group-vertical btn-group-lg" v-if="montrerListe">
-      <button
-        type="button"
-        class="btn btn-secondary"
-        v-for="theme in list_theme"
-        :key="theme.id"
-        @click="selectTheme(theme)"
-      >{{ theme.titre }}</button>
+  <div class="container">
+    <h1 class="theme-title m-5">LE SUPER QUIZZ MMI</h1>
+    <div class="row">
+      <div id="card" class="col-6">
+        <h3 class="text-left m-3">Thème</h3>
+        <button
+          id="boutton"
+          class="btn btn-primary m-5 col-8"
+          @click="montrerListe = !montrerListe"
+        >
+          <h2 class="m-auto">{{ theme ? theme.titre : "Choisir un thème..." }}</h2>
+        </button>
+        <div class="d-flex flex-column mt-n4 mb-5" v-if="montrerListe">
+          <button
+            id="boutton2"
+            type="button"
+            class="col-3 mx-auto btn my-2"
+            v-for="theme in list_theme"
+            :key="theme.id"
+            @click="selectTheme(theme)"
+          >{{ theme.titre }}</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -41,5 +50,38 @@ export default {
 <style scoped>
 .theme-title {
   margin-right: 1rem;
+}
+
+#card {
+  border-radius: 20px;
+  background-color: #f5b0c2;
+}
+
+.container {
+  font-family: "Raleway", sans-serif;
+}
+
+h1 {
+  font-weight: 800;
+}
+
+h3 {
+  font-weight: 600;
+  color: white;
+}
+
+#boutton {
+  border: none;
+  border-radius: 20px;
+  background-color: #eb5787;
+  box-shadow: 13px 15px 12px -10px rgba(0, 0, 0, 0.1);
+}
+
+#boutton2 {
+  border-radius: 20px;
+  background-color: #f4c6db;
+  border: none;
+  box-shadow: inset 13px 15px 12px -20px rgba(0, 0, 0, 0.1);
+  font-weight: 500;
 }
 </style>
--- mmiquizzvue/src/components/listeQuiz.vue
+++ mmiquizzvue/src/components/listeQuiz.vue
@@ -1,12 +1,14 @@
 <template>
-  <div>
-    <div class="d-flex align-items-end quiz-title-bloc" v-if="theme">
-      <ul>
-        <li v-for="quiz in theme.listeQuiz" :key="quiz.id">
-          <a  @click="selectQuiz(quiz.id)" href="">{{quiz.titre}}</a>
-        </li>
-      </ul>
-
+  <div class="container">
+    <div class="row">
+      <div id="card" class="col-6 d-flex quiz-title-bloc flex-column" v-if="theme">
+        <h3 class="text-left m-3">Liste Quizz</h3>
+        <ul>
+          <li class="mt-n1" v-for="quiz in theme.listeQuiz" :key="quiz.id">
+            <a class="px-4" @click="selectQuiz(quiz.id)" href>{{quiz.titre}}</a>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -17,13 +19,11 @@ export default {
   name: "listeQuiz",
   props: ["idTheme"],
   data: function() {
-    return {
-      
-    };
+    return {};
   },
   computed: {
     theme: function() {
-      console.log(getTheme(this.idTheme))
+      console.log(getTheme(this.idTheme));
       return getTheme(this.idTheme);
     }
   },
@@ -45,5 +45,42 @@ export default {
 
 .quiz-title-text {
   margin: 0;
+}
+
+#card {
+  border-radius: 20px;
+  background-color: #73c4be;
+}
+@import url("https://fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,700,800&display=swap%27");
+
+.container {
+  font-family: "Raleway", sans-serif;
+}
+
+ul {
+  margin: 0 !important;
+  padding: 0 !important;
+}
+
+h3 {
+  font-weight: 600;
+  color: white;
+}
+
+li {
+  list-style: none;
+  margin: 30px;
+}
+
+a {
+  padding: 10px;
+  text-decoration: none;
+  border-radius: 20px;
+  background-color: #20a88e;
+  border: none;
+  box-shadow: 13px 15px 12px -10px rgba(0, 0, 0, 0.1);
+  font-weight: 500;
+  font-size: 1.2em;
+  color: white;
 }
 </style>
